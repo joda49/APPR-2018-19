b98b5826baa59828490381b51100caa540e4d64b
Urban <urban.skywalker@gmail.com>
2019-09-04 18:09:17 +0200

    se malo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -17,11 +17,14 @@ zaposlenost$Drzava <- gsub("^European.*", 'EU', zaposlenost$Drzava)
 #Tabela 2
 
 zaposlenost_skupaj <- read.csv('podatki/zaposlenost_skupaj.csv', encoding = 'UTF-8', na = c(':',''), 
-                        col.names = c('Leto', 'Drzava', 'age', 'unit', 'sex', 'popu', 'Procent'))
+                        col.names = c('Leto', 'Drzava', 'age', 'unit', 'Spol', 'popu', 'Procent'))
 zaposlenost_skupaj <- zaposlenost_skupaj[,-6]
-zaposlenost_skupaj <- zaposlenost_skupaj[,-5]
 zaposlenost_skupaj <- zaposlenost_skupaj[,-4]
 zaposlenost_skupaj <- zaposlenost_skupaj[,-3]
 
 zaposlenost_skupaj$Drzava <- gsub("^Germany.*", 'Germany', zaposlenost_skupaj$Drzava)
 zaposlenost_skupaj$Drzava <- gsub("^European.*", 'EU', zaposlenost_skupaj$Drzava)
+
+#Tabela 3
+
+zaposlenost_vse <- rbind(zaposlenost, zaposlenost_skupaj)
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,31 +1,29 @@
-library(ggplot2)
-library(ggvis)
-library(dplyr)
-library(rgdal)
-library(mosaic)
-library(maptools)
-library(ggmap)
-library(mapproj)
-library(munsell)
+source('lib/libraries.r', encoding = 'UTF-8')
 
-# Uvozimo zemljevid Sveta
-# source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
-source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
+## Uvozimo zemljevid Sveta
+## source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
+#source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
 
-svet <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
-                        "ne_50m_admin_0_countries") %>% fortify()
+#svet <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+#                        "ne_50m_admin_0_countries") %>% fortify()
 
 
-# Zemljevid sveta skrčimo na zemljevid Evrope
-europe <- filter(svet, CONTINENT == "Europe")
-europe <- filter(europe, long < 55 & long > -35 & lat > 30 & lat < 85)
+## Zemljevid sveta skrčimo na zemljevid Evrope
+#europe <- filter(svet, CONTINENT == "Europe")
+#europe <- filter(europe, long < 55 & long > -35 & lat > 30 & lat < 85)
 
-europe <- filter(europe, NAME != "Jersey")
-europe <- filter(europe, NAME != "Russia")
+#europe <- filter(europe, NAME != "Jersey")
+#europe <- filter(europe, NAME != "Russia")
 
-# Drzave v zemljevidu Evrope
-drzave <- unique(europe$NAME) 
-drzave <- as.data.frame(drzave, stringsAsFactors=FALSE) 
-names(drzave) <- "Drzava"
+## Drzave v zemljevidu Evrope
+#drzave <- unique(europe$NAME) 
+#drzave <- as.data.frame(drzave, stringsAsFactors=FALSE) 
+#names(drzave) <- "Drzava"
 
+#Graf zaposlenosti
+
+graf_zaposlenost <- ggplot(data = filter(zaposlenost_vse, Drzava == 'Slovenia' | Drzava == 'Austria' | Drzava == 'Croatia'),
+                           mapping = aes(x=Leto, y=Procent, color=Spol, Group=Drzava)) +
+  geom_line() +
+  facet_wrap(Drzava~., ncol = 4)
 
